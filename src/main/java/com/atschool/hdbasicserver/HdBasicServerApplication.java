6c2b4eabf0429e772511ab85a24fb092454211da
cj <572562107@qq.com>
2022-07-11 16:07:17 +0800

    version 1.0

---
+++ src/main/java/com/atschool/hdbasicserver/HdBasicServerApplication.java
@@ -0,0 +1,13 @@
+package com.atschool.hdbasicserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdBasicServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HdBasicServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atschool/hdbasicserver/HdBasicServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atschool.hdbasicserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdBasicServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
