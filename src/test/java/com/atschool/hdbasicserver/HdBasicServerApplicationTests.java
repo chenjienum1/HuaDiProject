ca6a511f4b80be1d4ccb26cc67c1797045cbad83
programmer-otto <82355363+programmer-otto@users.noreply.github.com>
2022-07-12 13:34:08 +0800

    11111

--- src/test/java/com/atschool/hdbasicserver/HdBasicServerApplicationTests.java
+++ src/test/java/com/atschool/hdbasicserver/HdBasicServerApplicationTests.java
@@ -1,11 +1,15 @@
 package com.atschool.hdbasicserver;
 
+import com.atschool.hdbasicserver.mapper.LoginMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HdBasicServerApplicationTests {
 
+    @Autowired
+        LoginMapper loginmapper;
     @Test
     void contextLoads() {
     }
