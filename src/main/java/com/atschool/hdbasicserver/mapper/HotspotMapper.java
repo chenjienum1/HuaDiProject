74ddedc7e9ee7b4b0c98adb9092da41409fd2e43
Vendetta <1042426076@qq.com>
2022-07-13 14:18:48 +0800

    version wsq1.1
    cj,mzx are pig

--- src/main/java/com/atschool/hdbasicserver/mapper/HotspotMapper.java
+++ src/main/java/com/atschool/hdbasicserver/mapper/HotspotMapper.java
@@ -1,11 +1,15 @@
 package com.atschool.hdbasicserver.mapper;
 
+import com.atschool.hdbasicserver.bean.Hotspot;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
 
 
 @Mapper
 public interface HotspotMapper {
 
     public void addHotspot(String title, String time, String picture, String content);
+
+    public List<Hotspot> getHotspot();
 }
