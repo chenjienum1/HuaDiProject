25c7879aa721d3e390ca7b2ddaaf8a1e6559cedc
cj <572562107@qq.com>
2022-07-14 14:44:17 +0800

    version 4.5

--- src/test/java/com/atschool/hdbasicserver/HdBasicServerApplicationTests.java
+++ src/test/java/com/atschool/hdbasicserver/HdBasicServerApplicationTests.java
@@ -2,6 +2,7 @@ package com.atschool.hdbasicserver;
 
 import com.atschool.hdbasicserver.bean.*;
 import com.atschool.hdbasicserver.mapper.*;
+import com.github.pagehelper.PageHelper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,22 +16,27 @@ class HdBasicServerApplicationTests {
     HotspotMapper hotspotMapper;
     @Test
     void contextLoads() {
-        List<Hotspot> hotspotList = hotspotMapper.getHotspot();
-        for (Hotspot h: hotspotList
-             ) {
-            System.out.println(h.toString());
-        }
+        for (int i=13;i<=20;i++){
+        hotspotMapper.addHotspot(""+i,"1-1-1",i+"",""+i);}
     }
 
 
     /**
      * limit index,pageSize
      * index:当前页的起始索引
-     * pageSize：每页
+     * pageSize：每页显示的条数
+     * pageNumber:当前页的页码
+     * index=(pageNumber-1)*pageSize
      */
+
+
+
     @Test
     void testPage(){
+        PageHelper.startPage(1,4);
+        List<Hotspot> hotspot = hotspotMapper.getHotspot();
 
+        hotspot.forEach(hot -> System.out.println(hot));
     }
 
 }
