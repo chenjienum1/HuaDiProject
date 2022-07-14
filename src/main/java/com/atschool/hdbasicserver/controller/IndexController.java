511998ec7fabc79e3729e441e40fb07e754142b3
chen <cyyyyjjj@163.com>
2022-07-14 15:01:15 +0800

    version 3.3.3 更新login

--- src/main/java/com/atschool/hdbasicserver/controller/IndexController.java
+++ src/main/java/com/atschool/hdbasicserver/controller/IndexController.java
@@ -22,8 +22,5 @@ public class IndexController {
     public String login2(){
         return "boot";
     }
-    @RequestMapping("/loginup")
-    public String login3(){
-        return "login_up";
-    }
+
 }
