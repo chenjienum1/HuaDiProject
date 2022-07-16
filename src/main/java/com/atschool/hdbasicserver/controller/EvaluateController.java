f3394367500c3404cd635cb0284c2dea03c5f9e8
cj <572562107@qq.com>
2022-07-16 15:01:02 +0800

    version 6.4

--- src/main/java/com/atschool/hdbasicserver/controller/EvaluateController.java
+++ src/main/java/com/atschool/hdbasicserver/controller/EvaluateController.java
@@ -2,16 +2,15 @@ package com.atschool.hdbasicserver.controller;
 
 
 import com.atschool.hdbasicserver.bean.Evaluate;
-import com.atschool.hdbasicserver.bean.Hotspot;
 import com.atschool.hdbasicserver.mapper.EvaluateMapper;
 import com.atschool.hdbasicserver.service.EvaluateService;
-import com.atschool.hdbasicserver.service.HotspotService;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 
@@ -22,17 +21,18 @@ public class EvaluateController {
 
     @Autowired
     EvaluateService evaluateService;
+
     @Autowired
     EvaluateMapper evaluateMapper;
 
     @PostMapping(value = "/insertEvaluate")
-    public String insert(@RequestParam("title")String title,
-                       @RequestParam("kind") String kind,
-                       @RequestParam("satisfaction")Integer satisfaction,
-                       @RequestParam("year")String time_year,
-                       @RequestParam("month")String time_month,
-                       @RequestParam("day")String time_day,
-                       @RequestParam("evaluate")String evaluate){
+    public String insert(HttpSession session, @RequestParam("title")String title,
+                         @RequestParam("kind") String kind,
+                         @RequestParam("satisfaction")Integer satisfaction,
+                         @RequestParam("year")String time_year,
+                         @RequestParam("month")String time_month,
+                         @RequestParam("day")String time_day,
+                         @RequestParam("evaluate")String evaluate){
 
         String time = time_year + "-" + time_month + "-" + time_day;
         evaluateMapper.addEvaluate(title,kind,satisfaction,time,evaluate);
