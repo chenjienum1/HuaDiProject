1531f2a90659c867a99207ea77af51bd9c611612
cj <572562107@qq.com>
2022-07-19 09:08:53 +0800

    version 9.1

--- src/main/java/com/atschool/hdbasicserver/controller/IndexController.java
+++ src/main/java/com/atschool/hdbasicserver/controller/IndexController.java
@@ -1,14 +1,52 @@
 package com.atschool.hdbasicserver.controller;
 
+import com.atschool.hdbasicserver.bean.*;
+import com.atschool.hdbasicserver.service.*;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+    private static final int PAGENUM=3;
 
-    @RequestMapping("/index")
-    public String index(){
+    @Autowired
+    HotspotService hotspotService;
+
+    @Autowired
+    AffairService affairService;
+
+    @Autowired
+    FundsService fundsService;
+
+    @Autowired
+    PropertyService propertyService;
+
+    @Autowired
+    ResourceService resourceService;
+
+    @Autowired
+    EvaluateService evaluateService;
+
+    @RequestMapping(value = {"/","/index"})
+    public String index(Model model){
+        PageInfo<Hotspot> hotspotPageInfo = hotspotService.listPage(1, PAGENUM);
+        PageInfo<Affair> affairPageInfo = affairService.listPage(1, PAGENUM);
+        PageInfo<Funds> fundsPageInfo = fundsService.listPage(1, PAGENUM);
+        PageInfo<Property> propertyPageInfo = propertyService.listPage(1, PAGENUM);
+        PageInfo<Resource> resourcePageInfo = resourceService.listPage(1, PAGENUM);
+        PageInfo<Evaluate> evaluatePageInfo = evaluateService.listPage(1, PAGENUM);
+
+        model.addAttribute("indexHot",hotspotPageInfo);
+        model.addAttribute("indexAff",affairPageInfo);
+        model.addAttribute("indexFunds",fundsPageInfo);
+        model.addAttribute("indexPro",propertyPageInfo);
+        model.addAttribute("indexRes",resourcePageInfo);
+        model.addAttribute("indexEva",evaluatePageInfo);
+
+
         return "index";
     }
 
